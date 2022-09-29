951ccaec3050bb4adf688ff1906e9e066208fbcf
Scott Kellum <scott@typetura.com>
2022-09-29 16:35:57 -0700

    Figuring out a global nav structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,17 @@ export default defineConfig({
   siteName: 'Civic Website', // Name your project here
   domain: 'gov', // `gov` or `mil`?
   language: 'en', // `en` for English, `es` for Spanish
+  menu: {
+    'has submenu': {
+      'item one': '#item',
+      'item two': '#item',
+      'item three': '#item',
+      'item four': '#item',
+      'item five': '#item',
+      'item six': '#item',
+      'item seven': '#item',
+    },
+    'standalone link': '#link',
+  },
+  search: false,
 });
