58c905306e4c5a9487e4e88e0ddeec1599a8d45f
Scott Kellum <scott@typetura.com>
2022-12-13 21:53:32 -0800

    Add form data js

---
+++ src/scripts/formData.js
@@ -0,0 +1,10 @@
+function formData(obj) {
+    const formElements = Array.from(obj.elements);
+    const formValues = {};
+    formElements.forEach(element => {
+        if(element.name) {
+            formValues[element.name] = element.value;
+        }
+    });
+    return formValues;
+}
